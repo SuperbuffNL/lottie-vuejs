a82cced136bcda949f06495b8478703dae776b8b
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2021-08-09 00:44:52 +0700

    remove useless mixin

--- src/wrapper.js
+++ src/wrapper.js
@@ -4,10 +4,5 @@ import LottieAnimation from './LottieAnimation.vue';
 export default {
 	install(Vue, options = {}) {
 		Vue.component('lottie-animation', LottieAnimation);
-		Vue.mixin({
-			mounted() {
-				//console.log('lottie-animation installed!')
-			}
-		})
 	}
 };
